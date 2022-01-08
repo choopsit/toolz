a6accaa0019fa25c952d263cf1f4c89ba4ba54de
Choops <choopsbd@gmail.com>
2022-01-08 01:07:50 +0100

    Add functions

--- backup.py
+++ backup.py
@@ -28,7 +28,7 @@ warning = f"{cw}W{c0}:"
 def usage(err_code=0):
     my_script = os.path.basename(__file__)
     print(f"{ci}{__description__}\nUsage{c0}:")
-    print(f"  {my_script} [OPTION] <BACKUP_FOLDER>")
+    print(f"  {my_script} [OPTION]")
     print(f"{ci}Options{c0}:")
     print(f"  -h,--help: Print this help\n")
     exit(err_code)
@@ -42,7 +42,7 @@ def test_backupfolder(folder):
 
     if folder in forbiddens:
         print(f"{error} Invalid folder '{folder}'\n")
-        exit(1)
+        return False
 
     okcreate=""
 
@@ -51,19 +51,38 @@ def test_backupfolder(folder):
         okcreate = input("Create it [Y/n] ? ")
 
         if okcreate.lower() in ["n", "no"]:
-            exit(0)
+            return False
         else:
             os.makedirs(folder)
             os.chmod(folder, 0o777)
-
-    if not os.path.isdir(folder):
+    elif not os.path.isdir(folder):
         print(f"{error} '{folder}' exists but is not a folder\n")
-        exit(1)
+        return False
 
     return True
 
 
-def backup(dest_folder):
+def rsync_bkp(bkp_src, bkp_folder):
+    err_cpt = 0
+    bkp_subfolder = os.path.dirname(bkp_src)
+    rsync_opt = "-qOatzulr --delete --exclude='*~'"
+
+    if not os.path.exists(f"{bkp_folder}{bkp_subfolder}"):
+        os.makedirs(f"{bkp_folder}{bkp_subfolder}")
+
+    bkp_dst = f"{bkp_folder}/{bkp_subfolder}"
+    bkp_cmd = f"rsync {rsync_opt} {bkp_src} {bkp_dst}"
+
+    if os.system(bkp_cmd) != 0:
+        err_cpt += 1
+
+    return err_cpt
+
+
+def backup_to(dst_folder):
+    today = datetime.datetime.today().strftime("%y%m")
+    bkp_folder = f"{dst_folder}/bkp_{today}_{my_hostname}"
+
     home_backups = [".profile", ".face", ".kodi", ".mozilla", ".vim", ".steam",
             ".config", ".local", "Documents", "Music", "Pictures", "Videos",
             "Templates", "Work", "Games"]
@@ -79,16 +98,11 @@ def backup(dest_folder):
     if socket.gethostname() == "mrchat":
         more_backups = ["/volumes/speedix/Music", "/volumes/speedix/Games"]
 
-    today = datetime.datetime.today().strftime("%y%m")
-    bkp_folder = f"{dest_folder}/{today}_{socket.gethostname()}"
-
     if not os.path.exists(bkp_folder):
         os.makedirs(bkp_folder)
 
     my_user = getpass.getuser()
     my_home = pathlib.Path.home()
-
-    rsync_cmd = "rsync -qOatzulr --delete --exclude='*~'"
 
     err_home = 0
 
@@ -96,16 +110,7 @@ def backup(dest_folder):
         bkp_src = f"{my_home}/{bkp_element}"
 
         if os.path.exists(bkp_src):
-            bkp_subfolder = os.path.dirname(bkp_src)
-
-            if not os.path.exists(f"{bkp_folder}{bkp_subfolder}"):
-                os.makedirs(f"{bkp_folder}{bkp_subfolder}")
-
-            bkp_dst = f"{bkp_folder}/{bkp_subfolder}"
-            bkp_cmd = f"{rsync_cmd} {bkp_src} {bkp_dst}"
-
-            if os.system(bkp_cmd) != 0:
-                err_home += 1
+            err_home += rsync_bkp(bkp_src, bkp_folder)
 
     if err_home == 0:
         ecol = "\33[32m"
@@ -121,16 +126,7 @@ def backup(dest_folder):
 
     for bkp_src in cfg_files:
         if os.path.exists(bkp_src):
-            bkp_subfolder = os.path.dirname(bkp_src)
-
-            if not os.path.exists(f"{bkp_folder}{bkp_subfolder}"):
-                os.makedirs(f"{bkp_folder}{bkp_subfolder}")
-
-            bkp_dst = f"{bkp_folder}/{bkp_subfolder}"
-            bkp_cmd = f"{rsync_cmd} {bkp_src} {bkp_dst}"
-
-            if os.system(bkp_cmd) != 0:
-                err_cfg += 1
+            err_cfg += rsync_bkp(bkp_src, bkp_folder)
 
     if err_cfg == 0:
         ecol = "\33[32m"
@@ -145,19 +141,12 @@ def backup(dest_folder):
     for bkp_src in more_backups:
         if os.path.exists(bkp_src):
             err_other = 0
-            ecol = "\33[32m"
-            bkp_subfolder = os.path.dirname(bkp_src)
 
-            if not os.path.exists(f"{bkp_folder}{bkp_subfolder}"):
-                os.makedirs(f"{bkp_folder}{bkp_subfolder}")
+            err_other += rsync_bkp(bkp_src, bkp_folder)
 
-            bkp_dst = f"{bkp_folder}{bkp_subfolder}"
-            bkp_cmd = f"{rsync_cmd} {bkp_src} {bkp_dst}"
-
-            if os.system(bkp_cmd) != 0:
-                err_other += 1
-
-            if err_other != 0:
+            if err_other == 0:
+                ecol = "\33[32m"
+            else:
                 ecol = "\33[31m"
 
             more_bkp_ret = f"{done} '{bkp_src}' backuped in '{bkp_folder}'"
@@ -171,13 +160,23 @@ def backup(dest_folder):
 if __name__ == "__main__":
     if any(arg in sys.argv for arg in ["-h","--help"]):
         usage()
-    elif len(sys.argv) == 1:
-        print(f"{error} Need an argument\n")
-        usage(1)
-    elif len(sys.argv) == 2 and test_backupfolder(sys.argv[1]):
-        req_pkgs = ["rsync"]
-        toolz.pkg.prerequisites(req_pkgs)
-        backup(sys.argv[1])
-    else:
+    elif len(sys.argv) > 1:
         print(f"{error} Bad argument\n")
         usage(1)
+
+    dst_folder = ""
+    my_hostname = socket.gethostname()
+
+    if my_hostname == "mrchat":
+        dst_folder = "/backup"
+    elif my_hostname == "coincoin":
+        dst_folder = "choops@mrchat:/backup"
+    else:
+        dst_folder = input("Destination ? ")
+
+    if test_backupfolder(dst_folder):
+        req_pkgs = ["rsync"]
+
+        toolz.pkg.prerequisites(req_pkgs)
+
+        backup_to(dst_folder)
